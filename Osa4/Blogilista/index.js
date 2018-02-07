02c3eb43a45ca1385ce520b6ed80bc66c5fee489
Savipulu <jouni.winter@gmail.com>
2018-02-07 19:49:59 +0200

    Tehtävä 4.2

--- Osa4/Blogilista/index.js
+++ Osa4/Blogilista/index.js
@@ -3,45 +3,16 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogsRouter = require("./controllers/blogs");
 
-app.use(cors());
-app.use(bodyParser.json());
-
-/*
-  TODO:
-
-  CONFIGURE MONGOURL TO A DATABASE IN MLAB, WILL NOT WORK WITHOUT IT
-*/
 const mongoUrl =
   "mongodb://Savipulu:TietoKantaSalaSana@ds117178.mlab.com:17178/fullstack-notes";
 mongoose.connect(mongoUrl);
 mongoose.Promise = global.Promise;
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({})
-    .then(blogs => {
-      response.json(blogs);
-    })
-    .catch(error => {
-      console.log(error);
-      response.status(418).end();
-    });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    })
-    .catch(error => {
-      console.log(error);
-      response.status(418).end();
-    });
-});
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api/blogs", blogsRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
