6ac3c647ede2e4c33e783aecc609349af40bc851
Savipulu <jouni.winter@gmail.com>
2018-02-18 13:53:01 +0200

    Tehtävä 4.20

--- Osa4/Blogilista/controllers/blogs.js
+++ Osa4/Blogilista/controllers/blogs.js
@@ -3,6 +3,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
+/*
 const getTokenFrom = request => {
   const authorization = request.get('authorization')
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
@@ -10,6 +11,7 @@ const getTokenFrom = request => {
   }
   return null
 }
+*/
 
 blogsRouter.get('/', async (request, response) => {
   try {
@@ -28,10 +30,10 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
   try {
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
+    //const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-    if (!token || !decodedToken.id) {
+    if (!request.token || !decodedToken.id) {
       return response.status(401).json({ error: 'token missing or invalid' })
     }
     if (body.title === undefined) {
