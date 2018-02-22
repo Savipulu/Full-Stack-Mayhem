18efccf799e9d747a891a48fbbf2041b0c86fb73
Savipulu <jouni.winter@gmail.com>
2018-02-22 21:28:05 +0200

    Tehtävä 5.13

--- Osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ Osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import SimpleBlog from './SimpleBlog'
 
-describe.only('<SimpleBlog />', () => {
-  it('renders content', () => {
-    const blog = {
-      title: "testaus on 'kivaa'",
-      author: 'testermaster',
-      likes: 0
-    }
+describe('<SimpleBlog />', () => {
+  const blog = {
+    title: "testaus on 'kivaa'",
+    author: 'testermaster',
+    likes: 0
+  }
 
+  it('renders content', () => {
     const blogComponent = shallow(
       <SimpleBlog blog={blog} onClick={console.log('')} />
     )
@@ -19,5 +19,18 @@ describe.only('<SimpleBlog />', () => {
     expect(infoDiv.text()).toContain(blog.title)
     expect(infoDiv.text()).toContain(blog.author)
     expect(likesDiv.text()).toContain(blog.likes)
+  })
+  it('clicking the button twice calls event handler twice', () => {
+    const mockHandler = jest.fn()
+
+    const blogComponent = shallow(
+      <SimpleBlog blog={blog} onClick={mockHandler} />
+    )
+
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
