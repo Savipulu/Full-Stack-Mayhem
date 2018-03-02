244826d4a8cf893d879b4ab3fef39b3e22ad9a78
Savipulu <jouni.winter@gmail.com>
2018-03-02 22:18:42 +0200

    Tehtävä 6.8

--- Osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
+++ Osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -2,19 +2,17 @@ import React from 'react'
 import { vote } from '../reducers/anecdoteReducer'
 import { notify, clearNotification } from '../reducers/notificationReducer'
 import Filter from './Filter'
+import { connect } from 'react-redux'
 
 class AnecdoteList extends React.Component {
   render() {
-    const anecdotes = this.props.store.getState().anecdotes
     return (
       <div>
         <h2>Anecdotes</h2>
-        <Filter store={this.props.store} />
-        {anecdotes
+        <Filter />
+        {this.props.anecdotes
           .filter(a =>
-            a.content
-              .toLowerCase()
-              .includes(this.props.store.getState().filter.toLowerCase())
+            a.content.toLowerCase().includes(this.props.filter.toLowerCase())
           )
           .sort((a, b) => b.votes - a.votes)
           .map(anecdote => (
@@ -24,14 +22,9 @@ class AnecdoteList extends React.Component {
                 has {anecdote.votes}
                 <button
                   onClick={() => {
-                    this.props.store.dispatch(vote(anecdote.id))
-                    this.props.store.dispatch(
-                      notify(`you voted '${anecdote.content}'`)
-                    )
-                    setTimeout(
-                      () => this.props.store.dispatch(clearNotification()),
-                      5000
-                    )
+                    this.props.vote(anecdote.id)
+                    this.props.notify(`you voted '${anecdote.content}'`)
+                    setTimeout(() => this.props.clearNotification(), 5000)
                   }}
                 >
                   vote
@@ -44,4 +37,23 @@ class AnecdoteList extends React.Component {
   }
 }
 
-export default AnecdoteList
+const mapStateToProps = state => {
+  return {
+    anecdotes: state.anecdotes,
+    filter: state.filter
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    vote: id => {
+      dispatch(vote(id))
+    },
+    notify: value => {
+      dispatch(notify(value))
+    },
+    clearNotification: () => dispatch(clearNotification())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AnecdoteList)
