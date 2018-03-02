ee942cb5540845bed6f127a90cec18c7e53665db
Savipulu <jouni.winter@gmail.com>
2018-03-02 15:47:19 +0200

    Tehtävä 6.2

--- Osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
+++ Osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { vote } from '../reducers/anecdoteReducer'
 
 class AnecdoteList extends React.Component {
   render() {
@@ -6,21 +7,19 @@ class AnecdoteList extends React.Component {
     return (
       <div>
         <h2>Anecdotes</h2>
-        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote => (
           <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
+            <div>{anecdote.content}</div>
             <div>
               has {anecdote.votes}
-              <button onClick={() => 
-                this.props.store.dispatch({ type: 'VOTE', id: anecdote.id })
-              }>
+              <button
+                onClick={() => this.props.store.dispatch(vote(anecdote.id))}
+              >
                 vote
               </button>
             </div>
           </div>
-        )}
+        ))}
       </div>
     )
   }
--- Osa6/redux-anecdotes-v2/src/reducers/anecdoteReducer.js
+++ Osa6/redux-anecdotes-v2/src/reducers/anecdoteReducer.js
@@ -40,4 +40,11 @@ export const createAnecdote = content => {
   }
 }
 
+export const vote = id => {
+  return {
+    type: 'VOTE',
+    id
+  }
+}
+
 export default reducer
